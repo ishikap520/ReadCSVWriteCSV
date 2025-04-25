f4b0acb5f455d909c4b0139bbda7fa566b5e00fd
ishikap520 <ishikap520@gmail.com>
2025-04-24 20:00:49 -0500

    ReadCSVWriteCSV.cpp

--- ReadCSVWriteCSV/ReadCSVWriteCSV.cpp
+++ ReadCSVWriteCSV/ReadCSVWriteCSV.cpp
@@ -7,8 +7,8 @@ int main()
     ofstream empout;
     string in;
     string out;
-    string line, word;
-    string lastname, firstname, position, location;
+    string line;
+    string sentence, lastname, firstname, position, location;
     int age;
     vector<string> temp;
     vector<vector<string>> emp;
@@ -20,23 +20,6 @@ int main()
     e1.ReadFromFile();
     e1.AddToRow(line);
     e1.AddToLine();
-    if (empin.is_open())
-    {
-        while (getline(empin, line))
-        {
-            empout << line << endl;
-            stringstream gust(line);
-            while (getline(gust, word))
-            {
-                empout << setw(17) << word;
-            }
-            empout << endl;
-        }
-    }
-    else
-    {
-        cout << "Wrong!!" << endl;
-    }
     e1.WriteToFile();
     e1.CloseFiles();
 }
